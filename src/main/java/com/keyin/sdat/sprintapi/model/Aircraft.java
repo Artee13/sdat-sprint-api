083c64ef8c31d9258c120b14a53aa7c3a207ab9b
Artee13 <itemalook@gmail.com>
2026-01-14 15:23:50 -0330

    feat: add JPA entities and repositories with relationships

---
+++ src/main/java/com/keyin/sdat/sprintapi/model/Aircraft.java
@@ -0,0 +1,44 @@
+package com.keyin.sdat.sprintapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+public class Aircraft {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type;
+    private String airlineName;
+    private Integer numberOfPassengers;
+
+    @ManyToMany(mappedBy = "aircraftFlown")
+    @Builder.Default
+    private Set<Passenger> passengers = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "aircraft_takeoff_airports",
+            joinColumns = @JoinColumn(name = "aircraft_id"),
+            inverseJoinColumns = @JoinColumn(name = "airport_id")
+    )
+    @Builder.Default
+    private Set<Airport> takeoffAirports = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "aircraft_landing_airports",
+            joinColumns = @JoinColumn(name = "aircraft_id"),
+            inverseJoinColumns = @JoinColumn(name = "airport_id")
+    )
+    @Builder.Default
+    private Set<Airport> landingAirports = new HashSet<>();
+}
---
+++ src/main/java/com/keyin/sdat/sprintapi/repository/AirportRepository.java
@@ -0,0 +1,6 @@
+package com.keyin.sdat.sprintapi.repository;
+
+import com.keyin.sdat.sprintapi.model.Airport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AirportRepository extends JpaRepository<Airport, Long> {}
